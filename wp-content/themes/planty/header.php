cd8d29680931557989cce43c1b8eee16288ededf
Lou Villoing-Le Baron <louvlb05@gmail.com>
2025-02-07 15:10:44 +0100

    Nettoyage css ajout commentaires

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -18,6 +18,10 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap" rel="stylesheet">
+
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
